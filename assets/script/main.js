8300e8552ccc3a516e2d86e11c4bacdfd28facd9
xxdaniquee <xdaniquerijsdijk@gmail.com>
2023-12-14 09:58:24 +0100

    frontend button

--- assets/script/main.js
+++ assets/script/main.js
@@ -6,3 +6,16 @@ interaction.addEventListener('animationend', jumpHandler)
 function jumpHandler() {
   interaction.classList.toggle('jump')
 }
+
+
+// Frontend button
+
+let frontend = document.querySelector("a:nth-of-type(1)")
+
+frontend.addEventListener('click', fadeUpHandler)
+frontend.addEventListener('animationed', fadeUpHandler)
+
+function fadeUpHandler() {
+  frontend.classList.toggle('fade-up')
+}
+
